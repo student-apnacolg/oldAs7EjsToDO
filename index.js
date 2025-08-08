d1e72d3a8cb29ce67e7fd0aa2e57adb8541335fe
nikhil <nik70655@gmail.com>
2025-08-08 21:28:18 +0530

    rebuild

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
 const express = require('express')
 const app = express()
-const PORT = 8000;
+const PORT = 3233
 
-app.set("view engine","ejs")
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
 
-let todos = [];
+let tasks = []
+let editIndex = -1;
+let error = "";
 
-// GET: Show todo list
+// Get Todo List
 app.get('/', (req, res) => {
-  const filter = req.query.priority || "";
-  res.render('index', {todos, filter} )
+  res.render("list", {tasks, editIndex, error})
+  error = ''
 })
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   const task = req.body.task?.trim();
   const priority = req.body.priority;
 
+  const duplicate = tasks.some(t => t.text === task && t.priority === priority)
+  if (duplicate ) {
+    error="This task already exists!"
+    return res.redirect("/")
+  }
+
   if (task && priority) {
-    todos.push({text: task,priority})
-  } else {
-    console.log("Missing task or priority")
-  }
-  
-  res.redirect("/")
-})
-
-app.post("/delete", (req, res) => {
-  const index = req.body.index;
-  if(index !== undefined) {
-    todos.splice(index, 1)
+    tasks.push({text: task, priority})
   }
   res.redirect("/")
+  editIndex = -1
 })
 
-app.get("/edit", (req, res) => {
-  const index = req.query.index;
-  const todo = todos[index];
-
-  res.render("edit", { todo, index })
-})
-
-app.post("/edit", (req, res) => {
-  const index = req.body.index;
-  const updatedTask = req.body.updatedTask.trim();
-  const updatedPriority = req.body.updatedPriority || "Medium";
-
-  if (updatedTask && todos[index]) {
-    todos[index].text = updatedTask;
-    todos[index].priority = updatedPriority;
+app.post('/delete', (req, res) => {
+  const deleteIndex = req.body.deleteIndex;
+  if (deleteIndex !== undefined) {
+    tasks.splice(deleteIndex, 1)
   }
+  editIndex = -1
+  res.redirect('/')
+})
 
-  res.redirect("/");
+app.post("/start-edit", (req, res) => {
+  editIndex = Number(req.body.editIndex);
+  res.redirect('/')
+})
+
+app.post('/edit', (req, res) => {
+  const updatedTask = req.body.updatedIndex?.trim();
+  const updatedPriority = req.body.updatedPriority;
+  if (updatedTask && updatedPriority && editIndex !== -1) {
+    tasks[editIndex] = {
+      text: updatedTask,
+      priority: updatedPriority
+    }
+  }
+  editIndex = -1
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
